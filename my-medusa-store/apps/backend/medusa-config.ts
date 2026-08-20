2d828fe63505d5d62b549ae9df61f53ca645d5f0
fescobar-2 <39492866+fescobar-2@users.noreply.github.com>
2026-08-19 21:40:45 -0300

    Refactor port configuration in medusa-config.ts

--- my-medusa-store/apps/backend/medusa-config.ts
+++ my-medusa-store/apps/backend/medusa-config.ts
@@ -5,8 +5,8 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    port: process.env.PORT ? parseInt(process.env.PORT) : 9000,
     http: {
-      port: process.env.PORT ? parseInt(process.env.PORT) : 9000,
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
       authCors: process.env.AUTH_CORS!,
