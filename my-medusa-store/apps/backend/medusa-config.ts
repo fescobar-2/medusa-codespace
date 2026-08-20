897a6d534739f3cd61070e94b3131074ce53c86a
fescobar-2 <39492866+fescobar-2@users.noreply.github.com>
2026-08-20 19:06:57 -0300

    Remove server settings from medusa-config
    Removed server configuration from medusa-config.

--- my-medusa-store/apps/backend/medusa-config.ts
+++ my-medusa-store/apps/backend/medusa-config.ts
@@ -3,10 +3,6 @@ import { loadEnv, defineConfig } from '@medusajs/framework/utils'
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
-  server: {
-    port: 10000,
-    host: '0.0.0.0',
-  },
   admin: {
     disable: true,
   },
