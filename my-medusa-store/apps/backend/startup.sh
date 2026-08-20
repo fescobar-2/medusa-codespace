d8b8756b4ccbaf15725ac6bf3151a8fe6d3e1971
fescobar-2 <39492866+fescobar-2@users.noreply.github.com>
2026-08-20 20:23:15 -0300

    Add migration command to startup script

--- my-medusa-store/apps/backend/startup.sh
+++ my-medusa-store/apps/backend/startup.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+echo "Running migrations..."
+npx medusa db:migrate
+
 echo "Starting Medusa..."
-export PORT=${PORT:-9000}
-npx medusa start --port ${PORT:-10000} --host 0.0.0.0
+npx medusa start --port 10000 --host 0.0.0.0
