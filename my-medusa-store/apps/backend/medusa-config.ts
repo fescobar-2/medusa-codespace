1b997ba190449aa0364f93dd69a587a77c86055e
fescobar-2 <39492866+fescobar-2@users.noreply.github.com>
2026-08-20 22:32:45 -0300

    Update medusa-config.ts

--- my-medusa-store/apps/backend/medusa-config.ts
+++ my-medusa-store/apps/backend/medusa-config.ts
@@ -4,7 +4,7 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
   admin: {
-    disable: false,
+    disable: true,
   },
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
